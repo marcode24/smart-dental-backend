98264096efc4d2bffea14c60030894c877ca5543
marco cruz <marco24cruz08@gmail.com>
2022-06-08 21:53:58 -0500

    :bug: cors authorization fixed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
