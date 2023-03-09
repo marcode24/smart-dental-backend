1a3096e8db1dff29df7647d14c502c0621397dfb
marco cruz <marco24cruz08@gmail.com>
2023-03-08 18:55:46 -0600

    :heavy_plus_sign: add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -20,5 +20,46 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'max-len': ['error', { code: 90 }],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1,'maxBOF': 0 }],
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'semi': 'error',
+    'no-tabs': 'error',
+    'space-before-blocks': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: ['internal', 'external', 'builtin', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@nestjs/**',
+            group: 'external',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: 'src/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: 'test/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ]
+      },
+    ],
   },
 };
